4cc47ec91a379a6a48e828bfeb5889925108a51a
Fattiya Calista <Fattiyacalista9@gmail.com>
2024-07-29 23:51:51 +0700

    create transfer feautre

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ function SelectForm(){
                         <h5 className="text-sm text-gray-500">monitor where your money is going</h5>
                     </div>
                 </a>
-                <a className="border rounded p-4 w-full flex items-center space-x-4 bg-gray-50">
+                <a href="/form/transfer" className="border rounded p-4 w-full flex items-center space-x-4 hover:bg-gray-100 hover:border-gray-300">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-4">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
