fc9d3c285015b07e29878ac34b27f3069ede4193
Fattiya Calista <Fattiyacalista9@gmail.com>
2024-07-31 08:54:03 +0700

    add route redirect if not authenticate in form

---
+++ app/form/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google"
+import "@/app/globals.css";
+
+import { cn } from "@/lib/utils"
+import Header from "@/app/auth/components/header";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "Personal Expense Tracker",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const supabase = createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  console.log(user)
+
+  if(user == null){
+    redirect('/auth/login')
+  }
+
+  return (
+    <html lang="en">
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        {children}
+      </body>
+    </html>
+  );
+}
